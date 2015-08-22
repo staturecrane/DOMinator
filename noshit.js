4569164a3ff3677be8c33c279b6ec108ac0614c3
staturecrane <richard.alan.herbert@gmail.com>
2015-08-22 20:36:45 +0000

    refactored, added HTML attributes

--- noshit.js
+++ noshit.js
@@ -14,23 +14,41 @@ class noShit extends shitConstructor {
         super();
     }
     
-    createText(node, text = void 0){
-        let n = document.createElement(node.toUpperCase());
-        if (!!text){
-            let p = document.createTextNode(text);
-            n.appendChild(p);
+    createNode(node, text = void 0, options = void 0){
+        let opt = '';
+        if (!!options){
+            for (let x in options){
+                opt += (x + '=' + options[x] + ' ');
+            }    
         }
-        return n;
+        let n;
+        if (!!options){
+            n = '<' + node + ' ' + opt + '>';
+        }
+        else {
+            n = '<' + node + '>';
+        }
+        let div = document.createElement('div');
+        if (!!text){
+            n += text;
+        }
+        n += '</' + node + '>';
+        div.innerHTML = n;
+        var el = div.firstChild;
+        console.log(el);
+        return el;
+        
     }
     
-    create(node, text = void 0){
+    create(node, text = void 0, options = void 0){
         this.elements
-            .push(this.createText(node,text));
+            .push(this.createNode(node,text,options));
         return this;
     }
     
-    append(node, text = void 0, childLevel = this.childLevel){
+    append(node, text = void 0, options = void 0, childLevel = this.childLevel){
         let child = this.elements[this.elements.length-1];
+        console.log()
         if (!!child.children.length > 0 && this.childLevel > -1){
             child = child.children[childLevel];
         }
@@ -38,12 +56,12 @@ class noShit extends shitConstructor {
             child = child.parentNode;
         }
         child
-            .appendChild(this.createText(node,text));
+            .appendChild(this.createNode(node,text,options));
         return this;
     }
     
 
-    appendNth(node, text = void 0){
+    appendNth(node, text = void 0, options = void 0){
         function findChildNodes(child){
             if (child.childNodes.length > 0){
                 return findChildNodes(child.lastChild);
@@ -56,7 +74,7 @@ class noShit extends shitConstructor {
         this.childLevel += 1;
         let child = this.elements[this.elements.length-1].lastChild;
         findChildNodes(child)
-            .appendChild(this.createText(node,text));
+            .appendChild(this.createNode(node,text,options));
         return this;
     }
     
